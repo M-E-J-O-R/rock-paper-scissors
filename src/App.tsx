2e235969422dd551e81762e5b4403110b9aa1a8a
Mejor <amujobabatunde7@gmail.com>
2022-11-05 22:14:23 +0100

    feat: added logic to the rock paper scissors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './styles/app.css';
 import Home from './pages/Home';
-import Buttonn from './components/Buttonn';
 function App() {
   return (
     <div className="App">
       <Home />
-      <Buttonn/>
+    
 
 
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from 'react';
 import paper from '../assets/icon-paper.svg';
 import rock from '../assets/icon-rock.svg';
 import scissors from '../assets/icon-scissors.svg';
+
+
+
 const Home = () => {
+    const [score, setScore] = useState(0);
+
+    const [userPick, setUserPick] = useState('');
+  
+    let randomWeapon = Math.floor(Math.random() * 3);
+    const weaponArray: string[] = ['rock', 'paper', 'scissors'];
+
+    const [computerPick, setComputerPick] = useState(weaponArray[randomWeapon]);
+
+    let displayMessage:string
+
+    function handlePicks(element: string) {
+        randomWeapon  = Math.floor(Math.random()*3)
+        setUserPick(element);
+        setComputerPick(weaponArray[randomWeapon]);
+            
+        if (userPick === 'paper' && computerPick === 'rock') {
+            displayMessage = 'YOU WIN'
+        }
+        else if (userPick === 'paper' && computerPick === 'scissors') {
+            displayMessage= 'YOU LOSE'
+        }
+            
+        else if (userPick === 'rock' && computerPick === 'scissors') {
+            displayMessage = 'YOU WIN'
+        }
+        else if (userPick === 'rock' && computerPick === 'scissors') {
+            displayMessage= 'YOU LOSE'
+        }
+            
+        else if (userPick === 'scissors' && computerPick === 'paper') {
+            displayMessage = 'YOU WIN'
+        }
+        else if (userPick === 'scissors' && computerPick === 'rock') {
+            displayMessage= 'YOU LOSE'
+        }
+        else {
+            displayMessage ='DRAW'
+        }
+        
+
+        console.log(userPick, computerPick,displayMessage);
+    }
+    // useEffect(
+    //     () => {
+    //         //randomWeapon  = Math.floor(Math.random()*3)
+    //         console.log(randomWeapon);
+    //     }
+    // );
+
+
     return (<>
         <div className='Home'>
             <header>
@@ -16,27 +71,25 @@ const Home = () => {
                 </section>
             </header>
             <main>
+
                 <div className='game-board'>
-
-                    {/* <img className='game-icon paper' src={paper} alt="paper" />
-                <img className='game-icon scissors' src={scissors} alt="scissors" /> */}
-
-                    <div className="weapon-container paper">
-                        <img src={paper} className='weapon-icon' alt="" />
-                    </div>
+                    <>
+                        <div className="weapon-container paper" onClick={() => handlePicks('paper')}>
+                            <img src={paper} className='weapon-icon' alt="" />
+                        </div>
 
 
-                    <div className="weapon-container scissors">
-                        <img src={scissors} className='weapon-icon' alt="" />
-                    </div>
+                        <div className="weapon-container scissors" onClick={() => handlePicks('scissors')}>
+                            <img src={scissors} className='weapon-icon' alt="" />
+                        </div>
 
 
-                    <div className="weapon-container rock">
-                        <img src={rock} className='weapon-icon' alt="" />
-                    </div>
+                        <div className="weapon-container rock" onClick={() => handlePicks('rock')}
+                        >
+                            <img src={rock} className='weapon-icon' alt="" />
+                        </div>
 
-
-
+                    </>
                 </div>
 
 
